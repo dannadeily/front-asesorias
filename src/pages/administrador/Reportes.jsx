677ead8fd4b774c0710e9468126a4a8e768300af
danna duque <dannadeilydc@ufps.edu.co>
2023-05-27 21:14:14 -0500

    Se añade reportes y calificar

--- src/pages/administrador/Reportes.jsx
+++ src/pages/administrador/Reportes.jsx
@@ -285,7 +285,7 @@ const Reportes = () => {
                           </td>
                           <td className="px-6 py-4 whitespace-nowrap">
                             <div className="text-sm text-gray-900">
-                              {item.description}
+                              {item.estudiante}
                             </div>
                           </td>
                           <td className="px-6 py-4 whitespace-nowrap">
