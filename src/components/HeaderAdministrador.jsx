23e4a6094546f1580657d229ecc04c8db6c8c2a0
danna duque <dannadeilydc@ufps.edu.co>
2023-05-30 10:43:16 -0500

    arreglos en el header administrador

--- src/components/HeaderAdministrador.jsx
+++ src/components/HeaderAdministrador.jsx
@@ -32,77 +32,12 @@ const HeaderAdministrador = () => {
 
   return (
     <>
-      <nav className="bg-gray-50 dark:bg-red-700">
-        <div className="max-w-screen-xl px-4 py-3 md:px-6">
-          <button
-            className="text-gray-900 dark:text-white md:hidden"
-            onClick={handleClick}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </button>
+      <header className="fixed top-0 left-0 right-0 z-50">
+        <nav className="bg-gray-50 dark:bg-red-700">
           <div className="flex items-center justify-between">
-            <div className="flex items-center">
-              <ul
-                className={`${
-                  openMenu ? "block" : "hidden"
-                } md:block md:flex flex-col md:flex-row md:space-x-8 text-sm font-medium`}
-              >
-                <li>
-                  <Link
-                    to="/administrador/"
-                    className="block py-2 px-3 rounded-md hover:bg-transparent hover:text-blue-700 text-gray-900 dark:text-white"
-                  >
-                    Horario de asesorias
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="registrardocente"
-                    className="block py-2 px-3 rounded-md hover:bg-transparent hover:text-blue-700 text-gray-900 dark:text-white"
-                  >
-                    Registrar docente
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="horarioasesoria"
-                    className="block py-2 px-3 rounded-md hover:bg-transparent hover:text-blue-700 text-gray-900 dark:text-white"
-                  >
-                    Horario asesoria
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="listadocentes"
-                    className="block py-2 px-3 rounded-md hover:bg-transparent hover:text-blue-700 text-gray-900 dark:text-white"
-                  >
-                    Listado de docentes
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="reportes"
-                    className="block py-2 px-3 rounded-md hover:bg-transparent hover:text-blue-700 text-gray-900 dark:text-white"
-                  >
-                    Reportes
-                  </Link>
-                </li>
-              </ul>
-            </div>
-           
+            <h1 className="px-2 text-white font-bold text-1xl">
+              ADMINISTRADOR
+            </h1>
             <button
               className="flex items-center px-4 py-2 rounded ml-auto"
               onClick={handleLogout}
@@ -123,13 +58,83 @@ const HeaderAdministrador = () => {
               </svg>
               <span className="text-white">Salir</span>
             </button>
-            
-            
           </div>
-        </div>
-      </nav>
 
-      <main className="z-40">
+          <div className="max-w-screen-xl px-4 py-3 md:px-6">
+            <button
+              className="text-gray-900 dark:text-white md:hidden"
+              onClick={handleClick}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </button>
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <ul
+                  className={`${
+                    openMenu ? "block" : "hidden"
+                  } md:block md:flex flex-col md:flex-row md:space-x-8 text-sm font-medium`}
+                >
+                  <li>
+                    <Link
+                      to="/administrador/"
+                      className="block py-2 px-3 rounded-md hover:bg-transparent hover:text-blue-700 text-gray-900 dark:text-white"
+                    >
+                      Horario de asesorias
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="registrardocente"
+                      className="block py-2 px-3 rounded-md hover:bg-transparent hover:text-blue-700 text-gray-900 dark:text-white"
+                    >
+                      Registrar docente
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="horarioasesoria"
+                      className="block py-2 px-3 rounded-md hover:bg-transparent hover:text-blue-700 text-gray-900 dark:text-white"
+                    >
+                      Horario asesoria
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="listadocentes"
+                      className="block py-2 px-3 rounded-md hover:bg-transparent hover:text-blue-700 text-gray-900 dark:text-white"
+                    >
+                      Listado de docentes
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="reportes"
+                      className="block py-2 px-3 rounded-md hover:bg-transparent hover:text-blue-700 text-gray-900 dark:text-white"
+                    >
+                      Reportes
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </header>
+
+      <main className="z-40 mt-24">
         <Outlet />
       </main>
     </>
